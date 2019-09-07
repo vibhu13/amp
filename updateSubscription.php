cc99243fa5a40b3d87b3d2fac9f23106b56e6991
vibhu13 <smartyvibhuse@gmail.com>
2019-09-07 19:13:38 +0530

    Add files via upload

--- updateSubscription.php
+++ updateSubscription.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'headers.php';
+
 class Subscription {
 
     public function updateSubscription($email,$subscriptionFlag)  {
@@ -11,11 +13,11 @@ class Subscription {
            $conn = new PDO("mysql:host=$servername;dbname=amp", $username, $password);
            // set the PDO error mode to exception
            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           }
            $stmt = $conn->prepare("replace into amp.alert_subscription (email, subscription_type) values (:email , :subscription_type)");
            $stmt->bindParam(':email', $email);
            $stmt->bindParam(':subscription_type', $subscriptionFlag);
            $stmt->execute();
+	}
         catch(PDOException $e) {
            throw new Exception("Error While Updating Subscritption");
         }
@@ -28,3 +30,5 @@ $email = $_POST["email"];
 $subscriptionFlag = $_POST["subscriptionFlag"];
 $obj = new Subscription();
 $obj->updateSubscription($email,$subscriptionFlag);
+getHeaders();
+echo json_encode(true);
